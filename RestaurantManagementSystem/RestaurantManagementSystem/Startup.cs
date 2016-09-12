e3688c532d8f5282ac97a102c5ecd618dbcdd384
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-09-12 10:24:00 +1000

    setup base project templte

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantManagementSystem.Startup))]
+namespace RestaurantManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
